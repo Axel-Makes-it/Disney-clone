10472816432025eec93fff8dc2d4f2e18f46208c
Axel Ivan Santiago <95051495+Axel-Makes-it@users.noreply.github.com>
2023-10-19 23:13:27 -0400

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,14 +2,29 @@ import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 
 export default NextAuth({
-  // Configure one or more authentication providers
+  // Configure authentication providers
   providers: [
+    // Add your authentication providers here, e.g., GitHub, Google, etc.
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    // ...add more providers here
   ],
 
-  
+  // Add custom callbacks to control access
+  callbacks: {
+    async session(session, user) {
+      // Customize the session based on user properties, if needed
+      // For example, you can add a property to session based on user conditions
+
+      // In this example, we allow unauthenticated access to a "public" route
+      if (user) {
+        session.user = user;
+      } else {
+        session.user = { name: "Guest" };
+      }
+
+      return Promise.resolve(session);
+    },
+  },
 });
